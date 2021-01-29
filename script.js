e9e0fb8c4d8fc1183e32387b2e6240cc7ff7b8b0
amang007 <amangupta.ag36@gmail.com>
2021-01-29 23:50:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var boxOne = document.getElementsByClassName("box")[0];
+
+document.getElementsByClassName("toggleButton") 
+[0].onclick = function() {
+    if ( this.innerHTML === "Play"){
+        this.innerHTML = "Pause";
+        boxOne.classList.add("horizTranslate");
+    } else {
+        this.innerHTML = "Play";
+        var computedStyle = window.getComputedStyle(boxOne),
+        marginLeft = computedStyle.getPropertyValue("margin-left");
+        boxOne.style.marginLeft = marginLeft;
+        boxOne.classList.remove("horizTranslate");
+    }
+}
+
